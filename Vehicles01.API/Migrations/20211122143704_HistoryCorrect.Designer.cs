163e20a9c4f4f6cb3f407ae56ae4d13db94148a9
Ramiro quispe <ramiroquisper@gmail.com>
2021-12-09 10:16:30 -0500

    Eddit and delete procedures

--- Vehicles01.API/Migrations/20211122143704_HistoryCorrect.Designer.cs
+++ Vehicles01.API/Migrations/20211122143704_HistoryCorrect.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vehicles01.API.Data;
 
 namespace Vehicles01.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20211122143704_HistoryCorrect")]
+    partial class HistoryCorrect
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Vehicles01.API/Migrations/20211122143704_HistoryCorrect.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles01.API.Migrations
+{
+    public partial class HistoryCorrect : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Details_Histories_HistoryId",
+                table: "Details");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Details_Procedures_ProcedureId",
+                table: "Details");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Histories",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProcedureId",
+                table: "Details",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HistoryId",
+                table: "Details",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Histories_UserId",
+                table: "Histories",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Details_Histories_HistoryId",
+                table: "Details",
+                column: "HistoryId",
+                principalTable: "Histories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Details_Procedures_ProcedureId",
+                table: "Details",
+                column: "ProcedureId",
+                principalTable: "Procedures",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Histories_AspNetUsers_UserId",
+                table: "Histories",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Details_Histories_HistoryId",
+                table: "Details");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Details_Procedures_ProcedureId",
+                table: "Details");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Histories_AspNetUsers_UserId",
+                table: "Histories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Histories_UserId",
+                table: "Histories");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Histories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProcedureId",
+                table: "Details",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HistoryId",
+                table: "Details",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Details_Histories_HistoryId",
+                table: "Details",
+                column: "HistoryId",
+                principalTable: "Histories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Details_Procedures_ProcedureId",
+                table: "Details",
+                column: "ProcedureId",
+                principalTable: "Procedures",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
